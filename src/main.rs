00295b6d4f176ebf8a9dee73331102dfb2947f2e
Biskuit <gimlilegamer@gmail.com>
2025-08-02 12:54:01 +0200

    refactor and better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-use std::io;
-use std::error::Error;
-
-use crate::parser::{get_data_mon, get_base_stat, Pokemon};
-use crate::maths::{get_stat_number};
-
 mod maths;
 mod parser;
 
+use std::{io, str::FromStr};
 
+use crate::parser::Pokemon;
 
-fn main() {
-    
+fn main() -> anyhow::Result<()> {
     println!("Name of the attacking Pokémon ? : ");
     let mut name_attacking_mon: String = String::new();
 
-    io::stdin().read_line(&mut name_attacking_mon)
-        .expect("Failed to read line");
-    name_attacking_mon = name_attacking_mon.replace("\"", "").replace("\n", "").replace(" ", "").to_lowercase();
-    
-    let data_attacking_mon: Result<Pokemon, Box<dyn Error>> = get_data_mon(&name_attacking_mon);
-    println!("{data_attacking_mon:#?}");
-    let stat = get_base_stat(data_attacking_mon, "hp".to_string());
+    io::stdin().read_line(&mut name_attacking_mon)?;
+    name_attacking_mon = name_attacking_mon
+        .replace(char::is_whitespace, "")
+        .to_lowercase();
+
+    let attacking_mon = Pokemon::from_str(&name_attacking_mon)?;
+    println!("{attacking_mon:#?}");
+    let stat = attacking_mon.get_base_stat("hp".parse()?);
     println!("{stat}");
-    
+
+    Ok(())
 }
